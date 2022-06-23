29559cb68d97e44509cd8a272324178f698c0395
LeoGrasso2 <108083932+LeoGrasso2@users.noreply.github.com>
2022-06-23 13:07:28 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let cant=prompt('Ingrese la cantidad de alumnos del curso');
+Number.parseInt(cant, 10);
+
+for(let i=0; i<cant; i++){
+    
+    let nombre= prompt('Ingrese el nombre del alumno');
+    let nota= prompt('Ingrese su nota.');
+    Number.parseInt(nota, 10);
+    if(nota>10){
+        alert('Ingrese un numero no mayor a 10.')
+        i--;
+    }
+    else if(nota>=7){
+    alert("Felicidades para " +nombre + ', aprobó con ' + nota);
+    }
+    else if(nota<7){
+    alert('Lamentablemente, ' + nombre + ' desaprobó con ' + nota);
+    }
+    
+}
